1aa06401a23766cb9cce00e8996d68a850daf609
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2020-06-14 15:51:15 +0530

    Create ffmpeg.sh

---
+++ ffmpeg.sh
@@ -0,0 +1,15 @@
+apt update 
+apt upgrade
+apt install ffmpeg 
+mkdir compressor
+cd compressor
+wget -r -np -R "index.html*" 
+chmod +x crf14
+chmod +x crf18
+chmod +x crf24
+chmod +x crf28
+chmod +x crf32
+termux-serup-storage
+termux-setup-storage
+termux-setup-storage
+mkdir /sdcard/ffmpeg
